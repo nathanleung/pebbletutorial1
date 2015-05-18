05aa5981a2b4ffeff4e99b782f4f7f91bc7537e4
nathanleung <nathanleung23@gmail.com>
2015-05-17 23:46:45 -0400

    appmessage and pebblekit js intialize

---
+++ src/main.c
@@ -0,0 +1,162 @@
+#include <pebble.h>
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+
+static Window * s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+static TextLayer *s_weather_layer;
+
+static GFont s_time_font;
+static GFont s_weather_font;
+
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static void main_window_load(Window *window){
+  //Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0,0,144,168));
+  bitmap_layer_set_bitmap(s_background_layer,s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  //Create time TextLayer
+  s_time_layer = text_layer_create(GRect(5,52,139,50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer,GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  //Create date TextLayer
+  s_date_layer = text_layer_create(GRect(5,5,139,25));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer,GColorWhite);
+  text_layer_set_text(s_date_layer, "00/00/00");
+  //Create weather TextLayer
+  s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer,GColorWhite);
+  text_layer_set_text(s_weather_layer, "00:00");
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+  
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  //Imporve the layout to be more like a watch face
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+  // Create second custom font, apply it and add to Window
+  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+  text_layer_set_font(s_weather_layer, s_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  
+}
+
+static void main_window_unload(Window *window){
+  //Destory TextLayer
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
+  // Destroy weather elements
+text_layer_destroy(s_weather_layer);
+fonts_unload_custom_font(s_weather_font);
+  
+  fonts_unload_custom_font(s_time_font);
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
+}
+
+
+
+static void update_time(){
+  //Get a tm stuct
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  //Create a  long lived buffer
+  static char buffer[] = "00:00";
+  
+  //Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true){
+    //Use 24 hour format
+    strftime(buffer, sizeof("00:00"),"%H:%M",tick_time);
+  }else{
+    //Use 12 hour format
+    strftime(buffer,sizeof("00:00"),"%I:%M",tick_time);
+  }
+  //Display this time on the TextLayer
+  text_layer_set_text(s_time_layer,buffer);
+}
+
+static void update_date(){
+  //Get a tm stuct
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  //Create a  long lived buffer
+  static char buffer[] = "00/00/00";
+  
+    strftime(buffer, sizeof("00/00/00"),"%x",tick_time);
+  //Display this time on the TextLayer
+  text_layer_set_text(s_date_layer,buffer);
+  APP_LOG(APP_LOG_LEVEL_INFO, "updated date.");
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
+  update_time();
+}
+
+static void day_handler(struct tm *tick_time, TimeUnits units_changed){
+  update_date();
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context){
+  
+}
+static void inbox_dropped_callback(AppMessageResult reason, void *context){
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context){
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *interator, void *context){
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void init(){
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  //Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+  
+  window_set_window_handlers(s_main_window, (WindowHandlers){
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window,true);
+  
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(DAY_UNIT, day_handler);
+  update_time();
+  update_date();
+}
+
+static void deinit(){
+  //Destroy window
+  window_destroy(s_main_window);
+}
+
+int main(void){
+  init();
+  app_event_loop();
+  deinit();
+}
